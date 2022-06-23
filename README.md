ba1756b4760c4fe5c21abdefc10be4b3532407af
Scott Casey <scaseydv@gmail.com>
2022-06-23 15:33:47 -0600

    Initial page for publish test

---
+++ README.md
@@ -0,0 +1,84 @@
+<div id="top"></div>
+<span align="center">
+
+[![Contributors][contributors-shield]][contributors-url] [![Forks][forks-shield]][forks-url] [![Stargazers][stars-shield]][stars-url] [![Issues][issues-shield]][issues-url] [![MIT License][license-shield]][license-url]
+[![LinkedIn][linkedin-shield]][linkedin-url] [![Patreon][patreon-shield]][patreon-url]
+
+</span>
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+<a href="https://github.com/Kurohyou/timed-coding-quiz-sc">
+<img src="images/logo.png" alt="Logo" width="80" height="80">
+</a>
+<h3 align="center">Coding Quizzer</h3>
+<p align="center">
+A timed HTML coding quiz site with local storage of Scores and Times
+<br/>
+<a href="https://github.com/Kurohyou/timed-coding-quiz-sc"><strong>Explore the docs »</strong></a>
+<br/>
+<br/>
+<a href="https://github.com/Kurohyou/timed-coding-quiz-sc">View Demo</a>
+·
+<a href="https://github.com/Kurohyou/timed-coding-quiz-sc/issues">Report Bug</a>
+·
+<a href="https://github.com/Kurohyou/timed-coding-quiz-sc/issues">Request Feature</a>
+</p>
+</div>
+<!-- TABLE OF CONTENTS -->
+<details>
+<summary>Table of Contents</summary>
+<ol>
+<li>
+<a href="#about-the-project">About The Project</a>
+<ul>
+<li><a href="#built-with">Built With</a></li>
+</ul>
+</li>
+<li>
+<a href="#getting-started">Getting Started</a>
+<li><a href="#contact">Contact</a></li>
+</ol>
+</details>
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+[![Product Name Screen Shot][product-screenshot]]
+This coding quiz challenges users to beat their high scores with a variety of questions about HTML, CSS, and JS.
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+### Built With
+- Javascript
+- HTML (PUG)
+- CSS (SCSS)
+<p align="right">(<a href="#top">back to top</a>)</p>
+<!-- GETTING STARTED -->
+
+## Getting Started
+To test yourself, simply access the [deployed site]().
+<p align="right">(<a href="#top">back to top</a>)</p>
+<!-- USAGE EXAMPLES -->
+
+## Contact
+Scott Casey - [@your_twitter](https://twitter.com/Kurohyou) - scaseydv@gmail.com
+Project Link: [https://github.com/Kurohyou/timed-coding-quiz-sc](https://github.com/Kurohyou/timed-coding-quiz-sc)
+<p align="right">(<a href="#top">back to top</a>)</p>
+<!-- ACKNOWLEDGMENTS -->
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+
+[contributors-shield]: https://img.shields.io/github/contributors/Kurohyou/timed-coding-quiz-sc.svg?style=flat
+[contributors-url]: https://github.com/Kurohyou/timed-coding-quiz-sc/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/Kurohyou/timed-coding-quiz-sc.svg?style=flat
+[forks-url]: https://github.com/Kurohyou/timed-coding-quiz-sc/network/members
+[stars-shield]: https://img.shields.io/github/stars/Kurohyou/timed-coding-quiz-sc.svg?style=flat
+[stars-url]: https://github.com/Kurohyou/timed-coding-quiz-sc/stargazers
+[issues-shield]: https://img.shields.io/github/issues/Kurohyou/timed-coding-quiz-sc.svg?style=flat
+[issues-url]: https://github.com/Kurohyou/timed-coding-quiz-sc/issues
+[license-shield]: https://img.shields.io/github/license/Kurohyou/timed-coding-quiz-sc.svg?style=flat
+[license-url]: https://github.com/Kurohyou/timed-coding-quiz-sc/blob/master/LICENSE.txt
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=flat&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/Kurohyou
+[patreon-shield]: https://img.shields.io/endpoint.svg?url=https%3A%2F%2Fshieldsio-patreon.vercel.app%2Fapi%3Fusername%3Dkurohyoustudios%26type%3Dpatrons&style=flat
+[patreon-url]: https://patreon.com/kurohyoustudios
+[product-screenshot]: images/screenshot.png
---
+++ assets/javascript/quiz.js
@@ -0,0 +1,289 @@
+//quiz state object
+//stores the prompts that have been answered, whether they were wrong or right, and whether a quiz is in progress or not.
+const quiz = {
+  prompts:[],
+  answered:[],
+  clickFunction:()=>console.log('site not initialized'),
+  inProgress:false,
+  timerID:0,
+  startTime:0,
+  remainingTime:0,
+  time:60,
+  timeModifiers:0,
+  timerElements:{
+    clock: document.getElementById('clock'),
+    span: document.getElementById('clock-span')
+  }
+};
+
+const leaderBoard = JSON.parse(localStorage.getItem('leaderBoard')) || [];
+console.log(leaderBoard)
+//The start/continue button
+const contButton = document.getElementById('continue');
+const body = Array.from(document.getElementsByTagName('body'))[0];
+
+/**
+ * Progresses the timer display
+ * @param {number} [modifier = 0] - Amount of seconds to change the timer by
+ */
+const progressTime = (modifier = 0)=>{
+  quiz.remainingTime = Math.max(0,Math.ceil(quiz.time - ((Date.now() - quiz.startTime) / 1000)) + quiz.timeModifiers);
+  quiz.timerElements.clock.style.setProperty('--used',quiz.time - quiz.remainingTime);
+  quiz.timerElements.span.replaceChildren(quiz.remainingTime);
+  if(!modifier && quiz.remainingTime > 0){
+    //Set up the timer to be updated again in 1 second
+    quiz.timerID = setTimeout(progressTime,1000);
+  }else if(quiz.remainingTime <= 0){
+    finishQuiz();
+  }
+}
+
+/**
+ * Creates and starts the timer display
+ * @param {number} [duration = 60] - Duration of the quiz.
+ */
+const startTimer = (duration = 60)=>{
+  quiz.time = duration;
+  quiz.startTime = Date.now();
+  const clockContainer = Array.from(document.getElementsByClassName('clock-container'))[0];
+  clockContainer.style = '';
+  quiz.timerElements.clock.style.setProperty('--max',duration);
+  quiz.timerElements.clock.style.setProperty('--used',0);
+  progressTime(); 
+}
+
+/**
+ * Stops the timer from progressing
+ */
+const stopTimer = () => {
+  clearTimeout(quiz.timerID);
+  quiz.timerID = 0;
+}
+
+/**
+ * Randomizes the order of the queries
+ */
+const randomizeQueries = () => {
+  quiz.prompts = quiz.prompts.sort((a,b) => 0.5 - Math.random() );
+};
+//Listener Functions
+/**
+ * Starts the quiz
+ */
+const startQuiz = ()=>{
+  quiz.inProgress = true;
+  quiz.answered = [];
+  randomizeQueries();
+  //Change the clickFunction to the nextQuestion function
+  quiz.clickFunction = answerQuestion;
+  contButton.replaceChildren('Submit');
+  startTimer();
+  nextQuestion();
+}
+
+/**
+ * Collects the answer to a question, checks it, and updates the answer response
+ */
+const answerQuestion = ()=>{
+  const answer = Array.from(document.querySelectorAll('input[name="answer_option"]')).find(radio => radio.checked)?.value;
+  const currentPrompt = quiz.prompts[quiz.answered.length];
+  const answerIndex = currentPrompt.options.indexOf(answer);
+  let correct = answerIndex === currentPrompt.correctIndex;
+  quiz.answered.push(correct);
+  let result = 'correct!';
+  if(!correct){
+    result = 'incorrect!';
+    quiz.timeModifiers -= 5;
+  }
+  updateResultSpan(result,result.replace(/!/,''));
+  nextQuestion();
+};
+
+/**
+ * Checks the response to the prompt, and then continues to the next question.
+ */
+const nextQuestion = ()=>{
+  const prompt = quiz.prompts[quiz.answered.length];
+  if(!prompt){
+    //If we've finished the quiz, move to the finishing state.
+    finishQuiz();
+    return;
+  }
+  const span = clearQuestion();
+  insertQuestion(span,prompt);
+};
+
+/**
+ * Clears the previous question text
+ */
+const clearQuestion = () => {
+  const quizInterface = document.getElementById('quiz-interface');
+  const children = Array.from(quizInterface.children);
+  const span = children.shift();
+  return span;
+};
+
+/**
+ * resets the quiz interface to the initial state
+ */
+const resetQuiz = () => {
+  document.querySelector('#quiz-interface > span')
+    .replaceChildren('Click below to start the quiz. You will have 1 minute to complete the quiz. Wrong answers will deduct 5 seconds from the timer.');
+  document.getElementById('continue')
+    .replaceChildren('Take the Quiz!');
+  //Initialize the clickFunction to the startQuiz function
+  quiz.clickFunction = startQuiz;
+  contButton.removeAttribute('disabled');
+};
+
+/**
+ * Parses out basic markdown code syntax into html entities
+ * @param {string} text - The text to check for markdown syntax
+ */
+const parseCodeShorthand = (text) => {
+  console.log('text',text);
+  return text.replace(/`([^`]+?)`/g,'<pre><code>$1</code></pre>')
+}
+
+/**
+ * Inserts the prompt and options into the interface.
+ * @param {HTMLElement} interface - The section container for the quiz prompts
+ * @param {HTMLElement} span - The span that will hold the prompt text
+ * @param {object} prompt - Describes the prompt to be inserted
+ */
+const insertQuestion = (span,prompt) => {
+  span.innerHTML = parseCodeShorthand(prompt.prompt);
+  const optionDiv = document.getElementById('options');
+  optionDiv.replaceChildren();
+  prompt.options.forEach( (opt,index) => {
+    const answerCheck = document.createElement('input');
+    answerCheck.name = 'answer_option';
+    answerCheck.value = opt;
+    answerCheck.type = 'radio';
+    answerCheck.id = `answer-${index}`;
+    const answerLabel = document.createElement('label');
+    answerLabel.setAttribute('for',answerCheck.id);
+    answerLabel.append(opt);
+    optionDiv.append(answerCheck,answerLabel);
+  });
+};
+
+/**
+ * Finishes the quiz and creates the leaderboard name entry dialog
+ */
+const finishQuiz = () => {
+  stopTimer();
+
+  quiz.percentage = quiz.answered.filter(a => a).length / quiz.prompts.length * 100;
+
+
+  //Get the span to display the instructions and update the displayed text.
+  const noticeSpan = document.querySelector('#quiz-interface > span');
+  noticeSpan.innerHTML = `You finished the quiz in <strong>${quiz.time - quiz.remainingTime}</strong> seconds with a score of <strong>${quiz.percentage}%</strong>. Enter your initials below to save your results.`
+  
+  const options = document.getElementById('options');
+  const initialsInput = document.createElement('input');
+  initialsInput.type = 'text';
+  initialsInput.name = 'initials';
+  options.replaceChildren(initialsInput);
+
+  contButton.replaceChildren('Save Results');
+  quiz.clickFunction = saveResults;
+};
+//Listen for clicks on the continue button and call the function currently assigned to quiz.clickFunction
+contButton.addEventListener('click',()=>{
+  quiz.clickFunction();
+});
+
+/**
+ * 
+ * @param {string} text - Text to insert into the result span.
+ * @param {string} className - The class to add to the result span
+ */
+const updateResultSpan = (text,status)=>{
+  const resultSpan = document.getElementById('result');
+  resultSpan.replaceChildren(text);
+  resultSpan.className = `show ${status}`;
+  setTimeout(()=>{
+    resultSpan.className = '';
+  },3000,resultSpan)
+};
+
+const saveResults = ()=>{
+  const initials = document.querySelector('input[name="initials"]').value;
+  if(!initials || initials.length < 2){
+    updateResultSpan('Initials must be at least two characters','incorrect');
+  };
+  leaderBoard.push({initials,time:quiz.time - quiz.remainingTime,score:quiz.percentage});
+  localStorage.setItem('leaderBoard',JSON.stringify(leaderBoard));
+  updateLeaderBoard();
+  window.location.href = `${window.location.href.replace(/#.*/,'')}#leaderboard`;
+  resetQuiz();
+};
+
+/**
+ * Updates the leaderboard to show the current standings.
+ */
+const updateLeaderBoard = ()=>{
+  const aside = document.getElementById('leaderboard');
+  leaderBoard.sort((a,b) => b.score - a.score );
+  const ol = document.querySelector('#leaderboard > ol');
+  console.log(ol);
+  leaderBoard.forEach(obj => {
+    const li = document.createElement('li');
+    const flexSpan = document.createElement('span');
+    li.append(flexSpan);
+    //Create the spans for each data element of a leaderboard entry
+    const suffixLookup = {
+      initials:'',
+      time:'s',
+      score:'%'
+    };
+    Object.entries(suffixLookup).forEach( ([prop,suffix]) => {
+      const span = document.createElement('span');
+      const val = `${obj[prop]}${suffix}`;
+      span.append(val);
+      flexSpan.append(span);
+    });
+    ol.append(li);
+  });
+}
+
+//Get default
+
+// Create Listeners
+
+//Setup the quiz
+/**
+ * Function to grab the quiz data from the csv file. Uses the Papa Parse library to parse a csv file to get the prompt information.
+ */
+(async function(){
+  const github = window.location.href.match(/github/);
+  const prefix = github ? //Allows the js to pull from local version during development and the server version when live.
+    'https://raw.githubusercontent.com/Kurohyou/timed-coding-quiz-sc/main' :
+    '';
+  const data = await new Promise(resolve => {
+    Papa.parse(`${prefix}/data/questions.csv`,{
+      header:true,
+      download:true,
+      complete:results => resolve(results)
+    });
+  });
+  quiz.prompts = data.data
+    .filter((d) => d.prompt )
+    .map((d)=>{
+      return Object.entries(d)
+        .reduce((memo,[key,val]) => {
+          if(val && key.startsWith('option')){
+            memo.options.push(val);
+          }else if(val){
+            memo[key] = key === 'correctIndex' ?
+              +val :
+              val;
+          }
+          return memo;
+        },{options:[]});
+    });
+  resetQuiz();
+  updateLeaderBoard();
+})();
